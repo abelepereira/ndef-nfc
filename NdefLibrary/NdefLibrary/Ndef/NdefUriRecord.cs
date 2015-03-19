9f9c23c8f9d6a110fbd0d7d23057f255b658791c
abelepereira <abel.e.pereira@gmail.com>
2015-03-19 18:13:31 +0000

    Added NdefUriRecord ToString overload to get the record payload as a string.

--- NdefLibrary/NdefLibrary/Ndef/NdefUriRecord.cs
+++ NdefLibrary/NdefLibrary/Ndef/NdefUriRecord.cs
@@ -223,5 +223,16 @@ namespace NdefLibrary.Ndef
             if (record.Type == null) return false;
             return (record.TypeNameFormat == TypeNameFormatType.NfcRtd && record.Type.SequenceEqual(UriType));
         }
+
+		/// <summary>
+		/// Gets a string representation for the NDEF Record payload. 
+		/// </summary>
+		/// <returns>
+		/// A string that represents the current object.
+		/// </returns>
+		public override string ToString()
+		{
+			return Uri;
+		}
     }
 }
